792f6c5a33b793d163631c25fe69f054c0bb26e9
Matthew Epland <matthewepland@gmail.com>
2020-05-03 23:32:21 -0400

    switched from dogs to any 'nominal' class, added comparison stats for other 'non-nominal' classes, added min / max comparison plots - per minibatch

--- autoencoder_work/notebooks/autoencoder_imagenet.py
+++ autoencoder_work/notebooks/autoencoder_imagenet.py
@@ -19,9 +19,6 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 from sklearn.metrics import mean_squared_error
 
-output_path = '../output'
-models_path = '../models'
-
 
 # In[ ]:
 
@@ -36,6 +33,29 @@ print(f'device = {device}')
 
 batch_size=256
 im_res=128
+latent_dim = 7 # Latent Space size
+
+
+# In[ ]:
+
+
+print(f'Compressing from {im_res} x {im_res} to a latent dimension of {latent_dim} x {latent_dim}, ie shinking to {latent_dim}^2/{im_res}^2 = {latent_dim**2 / im_res**2:.2%} of the original size, before expanding back to {im_res} x {im_res}')
+
+
+# In[ ]:
+
+
+# potential good "nominal" classes
+nominal_classes = ['manhole_cover', 'car_wheel', 'barometer', 'bottlecap', 'lens_cap', 'puck', 'analog_clock', 'wall_clock', 'coffee_mug', 'coffeepot']
+nominal_class = nominal_classes[0]
+print(f'Running with nominal class: {nominal_class}')
+
+
+# In[ ]:
+
+
+output_path = f'../output/{nominal_class}_latent_dim_{latent_dim}'
+models_path = f'../models/{nominal_class}_latent_dim_{latent_dim}'
 
 
 # In[ ]:
@@ -71,8 +91,7 @@ pop_std0 = np.array([0.27657014, 0.27107376, 0.28344524])
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(pop_mean, pop_std0)])
 
-# ds_all_classes = tv.datasets.ImageFolder(root='C:/imagenet/processed_images/train', transform=transform)
-ds_all_classes = tv.datasets.ImageFolder(root='C:/imagenet/processed_images/train_subset_of_classes', transform=transform)
+ds_all_classes = tv.datasets.ImageFolder(root='C:/imagenet/processed_images/train', transform=transform)
 
 
 # In[ ]:
@@ -88,56 +107,31 @@ idx_to_class = OrderedDict([[v,k] for k,v in class_to_idx.items()])
 # In[ ]:
 
 
-# From https://www.kaggle.com/c/dog-breed-identification/data, plus a few extra
-possible_dog_classes = ['affenpinscher', 'afghan_hound', 'african_hunting_dog', 'airedale', 'american_staffordshire_terrier', 'appenzeller', 'australian_terrier', 'basenji', 'basset', 'beagle', 'bedlington_terrier', 'bernese_mountain_dog', 'black-and-tan_coonhound', 'blenheim_spaniel', 'bloodhound', 'bluetick', 'border_collie', 'border_terrier', 'borzoi', 'boston_bull', 'bouvier_des_flandres', 'boxer', 'brabancon_griffon', 'briard', 'brittany_spaniel', 'bull_mastiff', 'cairn', 'cardigan', 'chesapeake_bay_retriever', 'chihuahua', 'chow', 'clumber', 'cocker_spaniel', 'collie', 'curly-coated_retriever', 'dandie_dinmont', 'dhole', 'dingo', 'doberman', 'english_foxhound', 'english_setter', 'english_springer', 'entlebucher', 'eskimo_dog', 'flat-coated_retriever', 'french_bulldog', 'german_shepherd', 'german_short-haired_pointer', 'giant_schnauzer', 'golden_retriever', 'gordon_setter', 'great_dane', 'great_pyrenees', 'greater_swiss_mountain_dog', 'groenendael', 'ibizan_hound', 'irish_setter', 'irish_terrier', 'irish_water_spaniel', 'irish_wolfhound', 'italian_greyhound', 'japanese_spaniel', 'keeshond', 'kelpie', 'kerry_blue_terrier', 'komondor', 'kuvasz', 'labrador_retriever', 'lakeland_terrier', 'leonberg', 'lhasa', 'malamute', 'malinois', 'maltese_dog', 'mexican_hairless', 'miniature_pinscher', 'miniature_poodle', 'miniature_schnauzer', 'newfoundland', 'norfolk_terrier', 'norwegian_elkhound', 'norwich_terrier', 'old_english_sheepdog', 'otterhound', 'papillon', 'pekinese', 'pembroke', 'pomeranian', 'pug', 'redbone', 'rhodesian_ridgeback', 'rottweiler', 'saint_bernard', 'saluki', 'samoyed', 'schipperke', 'scotch_terrier', 'scottish_deerhound', 'sealyham_terrier', 'shetland_sheepdog', 'shih-tzu', 'siberian_husky', 'silky_terrier', 'soft-coated_wheaten_terrier', 'staffordshire_bullterrier', 'standard_poodle', 'standard_schnauzer', 'sussex_spaniel', 'tibetan_mastiff', 'tibetan_terrier', 'toy_poodle', 'toy_terrier', 'vizsla', 'walker_hound', 'weimaraner', 'welsh_springer_spaniel', 'west_highland_white_terrier', 'whippet', 'wire-haired_fox_terrier', 'yorkshire_terrier', 'dalmatian', 'coyote', 'timber_wolf', 'white_wolf',]
+idx_nominal = class_to_idx[nominal_class]
+idx_nominal_tensor = torch.tensor(ds_all_classes.targets) == idx_nominal
+ds_nominal = torch.utils.data.dataset.Subset(ds_all_classes, np.where(idx_nominal_tensor==1)[0])
 
 
 # In[ ]:
 
 
-imagenet_dog_classes = natsorted(list(set(class_to_idx.keys()).intersection(set(possible_dog_classes))))
+n_nominal_all = len(ds_nominal.indices)
 
-imagenet_dog_classes_idx = []
-for c in imagenet_dog_classes:
-    imagenet_dog_classes_idx.append(class_to_idx[c])
+n_nominal_test = int(0.15*n_nominal_all)
+n_nominal_val = int(0.15*n_nominal_all)
+n_nominal_train = n_nominal_all - n_nominal_test - n_nominal_val
 
+ds_nominal_test, ds_nominal_val, ds_nominal_train = torch.utils.data.random_split(ds_nominal, [n_nominal_test, n_nominal_val, n_nominal_train])
 
-# In[ ]:
-
-
-for i,class_idx in enumerate(imagenet_dog_classes_idx):
-    if i == 0:
-        idx_dogs = torch.tensor(ds_all_classes.targets) == class_idx
-    else:
-        idx_dogs += torch.tensor(ds_all_classes.targets) == class_idx
+del ds_nominal; ds_nominal = None;
 
 
 # In[ ]:
 
 
-ds_dogs = torch.utils.data.dataset.Subset(ds_all_classes, np.where(idx_dogs==1)[0])
-
-
-# In[ ]:
-
-
-n_dogs_all = len(ds_dogs.indices)
-
-n_dogs_test = int(0.15*n_dogs_all)
-n_dogs_val = int(0.15*n_dogs_all)
-n_dogs_train = n_dogs_all - n_dogs_test - n_dogs_val
-
-ds_dogs_test, ds_dogs_val, ds_dogs_train = torch.utils.data.random_split(ds_dogs, [n_dogs_test, n_dogs_val, n_dogs_train])
-
-del ds_dogs; ds_dogs = None;
-
-
-# In[ ]:
-
-
-dl_dogs_test = torch.utils.data.DataLoader(ds_dogs_test, batch_size=batch_size, shuffle=False, num_workers=8)
-dl_dogs_val = torch.utils.data.DataLoader(ds_dogs_val, batch_size=batch_size, shuffle=False, num_workers=8)
-dl_dogs_train = torch.utils.data.DataLoader(ds_dogs_train, batch_size=batch_size, shuffle=False, num_workers=8)
+dl_nominal_test = torch.utils.data.DataLoader(ds_nominal_test, batch_size=batch_size, shuffle=False, num_workers=8)
+dl_nominal_val = torch.utils.data.DataLoader(ds_nominal_val, batch_size=batch_size, shuffle=False, num_workers=8)
+dl_nominal_train = torch.utils.data.DataLoader(ds_nominal_train, batch_size=batch_size, shuffle=False, num_workers=8)
 
 
 # In[ ]:
@@ -159,8 +153,7 @@ class Autoencoder(nn.Module):
 
         self.relu = nn.ReLU()
 
-        # Latent Space size
-        latent_dim = 8
+        # Latent Space size, latent_dim, defined earlier
 
         # Encoder
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
@@ -225,10 +218,10 @@ optimizer = torch.optim.Adam(model.parameters(), weight_decay=1e-5)
 # In[ ]:
 
 
-dfp_train_results = train_model(dl_dogs_train, dl_dogs_val,
+dfp_train_results = train_model(dl_nominal_train, dl_nominal_val,
 model, optimizer, loss_fn, device,
 model_name='autoencoder', models_path=models_path,
-max_epochs=100, do_es=True, es_min_val_per_improvement=0.005, es_epochs=10,
+max_epochs=100, do_es=True, es_min_val_per_improvement=0.01, es_epochs=5,
 do_decay_lr=True, initial_lr=0.001, lr_epoch_period=30, lr_n_period_cap=6,
 )
 
@@ -267,18 +260,22 @@ plot_loss_vs_epoch(dfp_train_results, output_path, fname='loss_vs_epoch', tag=''
 # ***
 # # Eval
 
+# ### Load model from disk
+
 # In[ ]:
 
 
+best_epoch = 40
 model = Autoencoder()
-load_model(model, device, 40, 'autoencoder', models_path)
+load_model(model, device, best_epoch, 'autoencoder', models_path)
 
 
 # In[ ]:
 
 
-def eval_model(dl, model, loss_fn, loss_fn_no_reduction, device, m_path, fname='im_comp', tag='', n_comps_to_print=50, loss_type='\nLoss is MSE',
-               return_loss_stats=True, report_loss_per_class=True, dl_name=None,
+def eval_model(dl, model, loss_fn, loss_fn_no_reduction, device, m_path, fname='im_comp', tag='',
+               print_plots=True, n_comps_to_print=20, loss_type='\nLoss is MSE',
+               return_loss_stats=True, dl_name=None,
                idx_to_class=idx_to_class, mean_unnormalize=pop_mean, std_unnormalize=pop_std0):
     if not isinstance(loss_fn, nn.modules.loss.MSELoss):
         raise ValueError('Expected loss_fn == nn.MSELoss(), as individual loss annotation on numpy objects uses MSE. Update code and rerun!')
@@ -287,10 +284,11 @@ def eval_model(dl, model, loss_fn, loss_fn_no_reduction, device, m_path, fname='
     loss_arrays = []
     model.eval()
     with torch.no_grad():
-        eval_loss = get_loss(dl, model, loss_fn, device)
+        if print_plots:
+            eval_loss = get_loss(dl, model, loss_fn, device)
 
         i_comps = 0
-        for (images, classes) in dl:
+        for (images, classes) in tqdm(dl, desc='Minibatch'):
             # move labels to cpu
             classes_np = classes.numpy()
             class_arrays.append(classes_np)
@@ -305,126 +303,145 @@ def eval_model(dl, model, loss_fn, loss_fn_no_reduction, device, m_path, fname='
             loss_per_image = np.reshape(loss_per_pixel, (loss_per_pixel.shape[0], -1)).mean(axis=1)
             loss_arrays.append(loss_per_image)
 
-            # plot image comparisions, up to n_comps
-            i = 0
-            n_outputs = len(outputs)
-            while i < n_outputs and i_comps < n_comps_to_print:
-                idx = classes_np[i]
-                class_name = idx_to_class[idx]
+            if print_plots:
+                def _helper_plot_im_comp(i, _m_path, _fname, _tag='', _ann_add=''):
+                    class_name = idx_to_class[classes_np[i]]
 
-                im_orig = images[i].cpu().numpy()
-                im_pred = outputs[i].cpu().numpy()
+                    im_orig = images[i].cpu().numpy()
+                    im_pred = outputs[i].cpu().numpy()
 
-                # compute MSE loss in numpy
-                # this_loss = np.square(np.subtract(im_orig, im_pred)).mean()
+                    # compute MSE loss in numpy
+                    # this_loss = np.square(np.subtract(im_orig, im_pred)).mean()
 
-                # get from earlier calculation
-                this_loss = loss_per_image[i]
+                    # get from earlier calculation
+                    this_loss = loss_per_image[i]
 
-                plot_im_comp(im_orig, im_pred, m_path, fname, tag=f'_{i_comps}_{class_name}{tag}', inline=False,
-                             ann_text_std_add=f'Loss: {this_loss:.04f}\nMean Loss: {eval_loss:.04f}{loss_type}\n{class_name.title()}',
-                             mean_unnormalize=mean_unnormalize, std_unnormalize=std_unnormalize,
-                             ann_margin=True, left_right_orig_pred=True,
-                            )
+                    _tag = f'{_tag}_{class_name}{tag}'
 
-                i += 1; i_comps += 1;
+                    plot_im_comp(im_orig, im_pred, _m_path, _fname, tag=_tag, inline=False,
+                                 ann_text_std_add=f'Loss: {this_loss:.04f}\nMean Loss: {eval_loss:.04f}{loss_type}\n{class_name.title()}{_ann_add}',
+                                 mean_unnormalize=mean_unnormalize, std_unnormalize=std_unnormalize,
+                                 ann_margin=True, left_right_orig_pred=True,
+                                )
 
-    if return_loss_stats:
-        loss_array = np.concatenate(loss_arrays).ravel()
+                # plot image comparisions, up to n_comps
+                i = 0
+                n_outputs = len(outputs)
+                while i < n_outputs and i_comps < n_comps_to_print:
+                    _helper_plot_im_comp(i, m_path, fname, _tag=f'_{i_comps}', _ann_add='')
+                    i += 1; i_comps += 1;
 
-        def _get_l_stats(la, name=None):
-            l_mean = la.mean()
-            l_stddev = la.std()
-            l_min = la.min()
-            l_max = la.max()
-            l_median = np.median(la)
+                # plot image comparisions for max / min loss in this batch
+                i_min = loss_per_image.argmin()
+                l_min = loss_per_image[i_min]
+                _helper_plot_im_comp(i_min, f'{m_path}/mins', f'min_{l_min:.6f}_fname'.replace('.', '_'), _ann_add=f'\nMin Loss')
 
-            return {'name': name, 'l_mean': l_mean, 'l_stddev': l_stddev, 'l_min': l_min, 'l_max': l_max, 'l_median': l_median}
+                i_max = loss_per_image.argmax()
+                l_max = loss_per_image[i_max]
+                _helper_plot_im_comp(i_max, f'{m_path}/maxs', f'max_{l_max:.6f}_fname'.replace('.', '_'), _ann_add=f'\nMax Loss')
 
-        results = []
-        if report_loss_per_class:
+        if return_loss_stats:
+            loss_array = np.concatenate(loss_arrays).ravel()
             class_array = np.concatenate(class_arrays).ravel()
             idxs = natsorted(list(set(class_array)))
-            for idx in idxs:
+
+            def _get_l_stats(la, name=None):
+                return {
+                        'name': name, 'l_mean': la.mean(), 'l_stddev': la.std(), 'l_min': la.min(), 'l_max': la.max(),
+                        'l_median': np.median(la), 'n_images': la.size
+                       }
+
+            results = []
+            for idx in tqdm(idxs, desc='Class'):
                 this_loss_array = loss_array[np.where(class_array==idx)]
-                class_name = idx_to_class[idx]
-                results.append(_get_l_stats(this_loss_array, name=class_name))
+                results.append(_get_l_stats(this_loss_array, name=idx_to_class[idx]))
 
-        if dl_name is not None:
-            results.append(_get_l_stats(loss_array, name=dl_name))
+            if dl_name is not None:
+                results.append(_get_l_stats(loss_array, name=dl_name))
 
-        return pd.DataFrame(results)[['name', 'l_mean', 'l_stddev', 'l_min', 'l_max', 'l_median']]
+            return pd.DataFrame(results)[['name', 'l_mean', 'l_stddev', 'l_min', 'l_max', 'l_median', 'n_images']]
+
+
+# ### Get Stats on Nominal Class
+# Also plot some original / reconstructed image comparisons
+
+# In[ ]:
+
+
+# nominal
+dfp_nominal = eval_model(dl_nominal_val, model, loss_fn, loss_fn_no_reduction, device, f'{output_path}/comps/nominal')
+dfp_nominal['nominal'] = 1
 
 
 # In[ ]:
 
 
-# dogs
-dfp_dogs = eval_model(dl_dogs_val, model, loss_fn, loss_fn_no_reduction, device, f'{output_path}/comps/dogs', dl_name='dogs')
-dfp_dogs['dogs'] = 1
+dfp_nominal
+
+
+# ### Get Stats on NOT Nominal Classes
+
+# In[ ]:
+
+
+# not nominal
+ds_NOT_nominal = torch.utils.data.dataset.Subset(ds_all_classes, np.where(idx_nominal_tensor!=1)[0])
+dl_NOT_nominal = torch.utils.data.DataLoader(ds_NOT_nominal, batch_size=batch_size, shuffle=False, num_workers=8)
 
 
 # In[ ]:
 
 
-dfp_dogs
+# compute all not nominal class stats, but don't plot
+dfp_NOT_nominal = eval_model(dl_NOT_nominal, model, loss_fn, loss_fn_no_reduction, device, None, print_plots=False, dl_name='not_nominal')
+dfp_NOT_nominal['nominal'] = 0
+
+
+# Combine Results
+
+# In[ ]:
+
+
+dfp_class_results = pd.concat([dfp_nominal, dfp_NOT_nominal])
+dfp_class_results = massage_dfp(dfp_class_results, target_fixed_cols=['nominal', 'name', 'l_mean', 'l_stddev', 'l_min', 'l_max', 'l_median', 'n_images'],
+                                sort_by=['nominal', 'l_median'], sort_by_ascending=[False, True])
+write_dfp(dfp_class_results, output_path, 'class_results', tag='')
 
 
 # In[ ]:
 
 
-# not dogs, individually
-for _class,idx in class_to_idx.items():
-    if _class in possible_dog_classes:
-        continue
-    print(f'Processing {_class}')
+dfp_class_results.head(15)
 
+
+# In[ ]:
+
+
+dfp_class_results.tail(15)
+
+
+# ### Plot Image Comparisons for Select NOT Nominal Classes
+
+# In[ ]:
+
+
+# TODO
+# interesting_NOT_nominals = ['analog_clock', 'samoyed', 'scuba_diver']
+
+
+# In[ ]:
+
+
+pbar = tqdm(interesting_NOT_nominals)
+for _class in pbar:
+    pbar.set_description(f'Processing {_class}')
+
+    idx = class_to_idx[_class]
     this_idx = torch.tensor(ds_all_classes.targets) == idx
     ds = torch.utils.data.dataset.Subset(ds_all_classes, np.where(this_idx==1)[0])
     dl = torch.utils.data.DataLoader(ds, batch_size=batch_size, shuffle=False, num_workers=8)
 
     eval_model(dl, model, loss_fn, loss_fn_no_reduction, device, f'{output_path}/comps/{_class}', return_loss_stats=False)
-
-
-# In[ ]:
-
-
-# not dogs, together
-ds_NOT_dogs = torch.utils.data.dataset.Subset(ds_all_classes, np.where(idx_dogs!=1)[0])
-dl_NOT_dogs = torch.utils.data.DataLoader(ds_NOT_dogs, batch_size=batch_size, shuffle=False, num_workers=8)
-
-
-# In[ ]:
-
-
-dfp_NOT_dogs = eval_model(dl_NOT_dogs, model, loss_fn, loss_fn_no_reduction, device, None, n_comps_to_print=0, dl_name='not_dogs')
-dfp_NOT_dogs['dogs'] = 0
-
-
-# In[ ]:
-
-
-dfp_NOT_dogs
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-dfp_class_results = pd.concat([dfp_dogs, dfp_NOT_dogs])
-
-
-# In[ ]:
-
-
-write_dfp(dfp_class_results, output_path, 'class_results', tag='', target_fixed_cols=['name', 'l_mean', 'l_stddev', 'l_min', 'l_max', 'l_median'],
-          sort_by=None, sort_by_ascending=None,
-         )
 
 
 # ***
